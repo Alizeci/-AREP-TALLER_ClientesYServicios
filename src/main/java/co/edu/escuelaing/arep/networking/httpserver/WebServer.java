d0fc376e6027d49933bc7f47c9e7189cd9d4f93d
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2021-09-05 01:46:47 -0500

    Deploy to Heroku fixed

--- src/main/java/co/edu/escuelaing/arep/networking/httpserver/WebServer.java
+++ src/main/java/co/edu/escuelaing/arep/networking/httpserver/WebServer.java
@@ -12,143 +12,152 @@ import java.nio.file.Paths;
 
 public class WebServer {
 
-    private static final WebServer _instance = new WebServer();
+	private static final WebServer _instance = new WebServer();
 
-    public static WebServer getInstance() {
+	public static WebServer getInstance() {
 
-        return _instance;
-    }
+		return _instance;
+	}
 
-    private WebServer() {
+	private WebServer() {
 
-    }
+	}
 
-    public void startSocket(String[] args, int port) throws IOException, URISyntaxException {
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(port);
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: 35000.");
-            System.exit(1);
-        }
+	public void startSocket(String[] args, int port) throws IOException, URISyntaxException {
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: 35000.");
+			System.exit(1);
+		}
 
-        boolean running = true;
-        while (running) {
-            Socket clientSocket = null;
-            try {
-                System.out.println("Listo para recibir ...");
-                clientSocket = serverSocket.accept();
+		boolean running = true;
+		while (running) {
+			Socket clientSocket = null;
+			try {
+				System.out.println("Listo para recibir ...");
+				clientSocket = serverSocket.accept();
 
-            } catch (IOException e) {
-                System.err.println("Accept failed.");
-                System.exit(1);
-            }
-            serverConnection(clientSocket);
-        }
-        serverSocket.close();
-    }
+			} catch (IOException e) {
+				System.err.println("Accept failed.");
+				System.exit(1);
+			}
+			serverConnection(clientSocket);
+		}
+		serverSocket.close();
+	}
 
-    public void serverConnection(Socket clientSocket) throws IOException, URISyntaxException {
-        if (clientSocket != null) {
+	public void serverConnection(Socket clientSocket) throws IOException, URISyntaxException {
+		if (clientSocket != null) {
 
-            PrintWriter out;
-            BufferedReader in;
-            OutputStream los_outputStream;
-            InputStream lin_inputStream;
+			PrintWriter out;
+			BufferedReader in;
+			OutputStream los_outputStream;
+			InputStream lin_inputStream;
 
-            los_outputStream = clientSocket.getOutputStream();
-            lin_inputStream = clientSocket.getInputStream();
+			los_outputStream = clientSocket.getOutputStream();
+			lin_inputStream = clientSocket.getInputStream();
 
-            if (los_outputStream != null && lin_inputStream != null) {
-                String inputLine, outputLine;
-                StringBuilder request;
+			if (los_outputStream != null && lin_inputStream != null) {
+				String inputLine, outputLine;
+				StringBuilder request;
 
-                request = new StringBuilder();
-                out = new PrintWriter(los_outputStream, true); // envío de msgs al Cliente.
-                in = new BufferedReader(new InputStreamReader(lin_inputStream)); // recibir msgs del Cliente
+				request = new StringBuilder();
+				out = new PrintWriter(los_outputStream, true); // envío de msgs al Cliente.
+				in = new BufferedReader(new InputStreamReader(lin_inputStream)); // recibir msgs del Cliente
 
-                while ((inputLine = in.readLine()) != null) {
-                    // System.out.println("Received: " + inputLine);
-                    request.append(inputLine);
-                    if (!in.ready()) {
-                        break;
-                    }
-                }
-                String[] ls_request;
-                System.out.println("#"+request.toString()+"#");
-                ls_request = request.toString().split(" ");
-                
-                if ((ls_request != null)) {
-                	try {
-                		String ls_uriStr;
-                		ls_uriStr = ls_request[1];
-                		
-                		if ((ls_uriStr != null) && (!ls_uriStr.isEmpty())) {
-                			URI resourceURI;
-                			resourceURI = new URI(ls_uriStr);
-                			
-                			outputLine = getResource(resourceURI);
-                			out.println(outputLine);
-                		}	
-                	}catch(ArrayIndexOutOfBoundsException e) {
-                		System.out.println(ls_request.toString());
-                		throw new IOException("ServerConnection ls_request  "+ e.getMessage());
-                	}
+				if (in != null && in.ready()) {
 
-                }
-                // System.out.println("Received URI path: " + resourceURI.getPath());
-                // System.out.println("Received URI query: " + resourceURI.getQuery());
-                out.close();
-                in.close();
-            }
-            clientSocket.close();
-        } else {
-            throw new IOException("ServerConnection Socket no puede ser nulo");
-        }
-    }
+					while ((inputLine = in.readLine()) != null) {
+						// System.out.println("Received: " + inputLine);
+						request.append(inputLine);
+						if (!in.ready()) {
+							break;
+						}
+					}
+					String ls_request;
+					System.out.println("#" + request.toString() + "#");
 
-    // Probar localhost:35000/demo.html, este lee el .html, .css y .js
-    public String getResource(URI resourceURI) throws IOException {
+					ls_request = request.toString();
 
-        StringBuilder response = new StringBuilder();
-        Charset charset = Charset.forName("UTF-8");
-        Path htmlFile = Paths.get("target/classes/public" + resourceURI.getPath());
+					if ((ls_request != null) && (!ls_request.isEmpty())) {
 
-        try (BufferedReader in = Files.newBufferedReader(htmlFile, charset)) {
+						String ls_uriStr;
+						String[] las_request;
 
-            String str, type = null;
-            type = setMimeTypeContent(resourceURI.getPath());
-            response = new StringBuilder("HTTP/1.1 200 OK\r\n" + "Content-Type: " + type + "\r\n"); // Define
-            // MimeType
-            // of file
-            while ((str = in.readLine()) != null) {
-                response.append(str + "\n");
-            }
+						las_request = ls_request.split(" ");
 
-        } catch (IOException e) {
-            System.err.format("IOException: %s%n", e);
-            return default404Response();
-        }
+						if ((las_request != null)) {
 
-        return response.toString();
-    }
+							ls_uriStr = las_request[1];
 
-    private String setMimeTypeContent(String path) {
-        String type = null;
-        if (path.contains(".css")) {
-            type = "text/css";
-        } else if (path.contains(".html")) {
-            type = "text/html";
-        } else if (path.contains(".js")) {
-            type = "text/javascript";
-        }
-        return type;
-    }
+							if ((ls_uriStr != null) && (!ls_uriStr.isEmpty())) {
+								URI resourceURI;
+								resourceURI = new URI(ls_uriStr);
 
-    private String default404Response() {
-        String outputLine = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + "\r\n" + "<!DOCTYPE html>\n"
-                + "<html>\n" + "	<head>\n" + "		<meta charset=\"UTF-8\">\n" + "		<title>Error</title>\n"
-                + "	</head>\n" + "	<body>\n" + "		<h1>NOT FOUND 404</h1>\n" + "	</body>\n" + "</html>\n";
-        return outputLine;
-    }
+								outputLine = getResource(resourceURI);
+								out.println(outputLine);
+							}
+						}
+
+					}
+					// System.out.println("Received URI path: " + resourceURI.getPath());
+					// System.out.println("Received URI query: " + resourceURI.getQuery());
+					out.close();
+					in.close();
+				}else {					
+					throw new IOException("ServerConnection BufferReader input vacío o nulo!");
+				}
+			}
+			clientSocket.close();
+		} else {
+			throw new IOException("ServerConnection Socket no puede ser nulo");
+		}
+	}
+
+	// Probar localhost:35000/demo.html, este lee el .html, .css y .js
+	public String getResource(URI resourceURI) throws IOException {
+
+		StringBuilder response = new StringBuilder();
+		Charset charset = Charset.forName("UTF-8");
+		Path htmlFile = Paths.get("target/classes/public" + resourceURI.getPath());
+
+		try (BufferedReader in = Files.newBufferedReader(htmlFile, charset)) {
+
+			String str, type = null;
+			type = setMimeTypeContent(resourceURI.getPath());
+			response = new StringBuilder("HTTP/1.1 200 OK\r\n" + "Content-Type: " + type + "\r\n"); // Define
+			// MimeType
+			// of file
+			while ((str = in.readLine()) != null) {
+				response.append(str + "\n");
+			}
+
+		} catch (IOException e) {
+			System.err.format("IOException: %s%n", e);
+			return default404Response();
+		}
+
+		return response.toString();
+	}
+
+	private String setMimeTypeContent(String path) {
+		String type = null;
+		if (path.contains(".css")) {
+			type = "text/css";
+		} else if (path.contains(".html")) {
+			type = "text/html";
+		} else if (path.contains(".js")) {
+			type = "text/javascript";
+		}
+		return type;
+	}
+
+	private String default404Response() {
+		String outputLine = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + "\r\n" + "<!DOCTYPE html>\n"
+				+ "<html>\n" + "	<head>\n" + "		<meta charset=\"UTF-8\">\n" + "		<title>Error</title>\n"
+				+ "	</head>\n" + "	<body>\n" + "		<h1>NOT FOUND 404</h1>\n" + "	</body>\n" + "</html>\n";
+		return outputLine;
+	}
 }
