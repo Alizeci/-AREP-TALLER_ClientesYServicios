ad12ebe60202f14ff647182ec10ff6cd4cc85126
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2021-09-05 14:41:19 -0500

    Deploy to Heroku fixed

--- src/main/java/co/edu/escuelaing/arep/networking/httpserver/WebServer.java
+++ src/main/java/co/edu/escuelaing/arep/networking/httpserver/WebServer.java
@@ -55,25 +55,24 @@ public class WebServer {
 			BufferedReader in;
 			OutputStream los_outputStream;
 			InputStream lis_inputStream;
-			//InputStreamReader lir_inputStream;
 
 			los_outputStream = clientSocket.getOutputStream();
 			lis_inputStream = clientSocket.getInputStream();
 
 			if ((los_outputStream != null) && (lis_inputStream != null) && (lis_inputStream.available() > 0)) {
-				System.out.println("Entra bien 1");
+
 				String inputLine, outputLine;
 				StringBuilder request;
 
 				request = new StringBuilder();
 				out = new PrintWriter(los_outputStream, true); // envío de msgs al Cliente.
 				in = new BufferedReader(new InputStreamReader(lis_inputStream)); // recibir msgs del Cliente
+				
 				System.out.println("tiene lineas por leer: "+ in.ready());
 				if (in != null && in.ready()) {
-					System.out.println("Entra bien 2");
 
 					while ((inputLine = in.readLine()) != null) {
-						System.out.println("Received: " + inputLine);
+						//System.out.println("Received: " + inputLine);
 						request.append(inputLine);
 						if (!in.ready()) {
 							break;
@@ -92,22 +91,17 @@ public class WebServer {
 						las_request = ls_request.split(" ");
 
 						if ((las_request != null)) {
-
 							ls_uriStr = las_request[1];
-							System.out.println("ls_uriStr: "+ ls_uriStr);
 
 							if ((ls_uriStr != null) && (!ls_uriStr.isEmpty())) {
-								System.out.println("Entra bien 3");
 								URI resourceURI;
 								resourceURI = new URI(ls_uriStr);
 								System.out.println("resourceURI: "+ resourceURI);
 
 								outputLine = getResource(resourceURI);
-								System.out.println("outputLine: "+ outputLine);
 								out.println(outputLine);
 							}
 						}
-
 					}
 
 					out.close();
@@ -124,21 +118,22 @@ public class WebServer {
 
 	// Probar localhost:35000/demo.html, este lee el .html, .css y .js
 	public String getResource(URI resourceURI) throws IOException {
-
 		StringBuilder response = new StringBuilder();
+		
 		Charset charset = Charset.forName("UTF-8");
 		Path htmlFile = Paths.get("target/classes/public" + resourceURI.getPath());
 
 		try (BufferedReader in = Files.newBufferedReader(htmlFile, charset)) {
-
+			System.out.println("Entra!!!!!!!! ");
 			String str, type = null;
+			
 			type = setMimeTypeContent(resourceURI.getPath());
-			response = new StringBuilder("HTTP/1.1 200 OK\r\n" + "Content-Type: " + type + "\r\n"); // Define
-			// MimeType
-			// of file
+			response = new StringBuilder("HTTP/1.1 200 OK\r\n" + "Content-Type: " + type + "\r\n"); // Define MimeType of file
+			
 			while ((str = in.readLine()) != null) {
 				response.append(str + "\n");
 			}
+			System.out.println(response);
 
 		} catch (IOException e) {
 			System.err.format("IOException: %s%n", e);
